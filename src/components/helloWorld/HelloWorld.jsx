dc2d9b1a2a9648c60247a99ea5794410266fc62d
aeoliun <aeoliun@gmail.com>
2021-07-21 22:01:57 -0400

    created a disclaimer and github redirect to new tab

--- src/components/helloWorld/HelloWorld.jsx
+++ src/components/helloWorld/HelloWorld.jsx
@@ -2,6 +2,7 @@ export default function HelloWorld() {
 	return ( 
 		<div className="HelloWorld">
 			<p className="greeting">Hello, World.</p>
+			<p className="disclaimer">The portfolio is still under construction, <br/>but you can visit <span className="red"><a href="https://github.com/brianradon?tab=repositories" target="_blank">github.com/brianradon</a></span> to see my current projects</p>
 		</div>
 	)
 }
